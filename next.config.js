164dae64cad2c57e22d0c4aed3fe786c9880ba46
soleil00 <srukundo01@gmail.com>
2024-02-02 16:51:14 +0200

    generated my static html file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  // Your other Next.js configuration options go here
-
-  // Add the output property for static HTML export
-  output: {
-    // This will create a folder named "out" with the exported files
-    // Feel free to change the "out" to your preferred output folder name
-    dir: 'out',
-  },
-};
-
-module.exports = nextConfig;
+  output: 'export',
+    distDir: "dist",
+    images: {
+      unoptimized:true 
+  }
+}
+ 
+module.exports = nextConfig
