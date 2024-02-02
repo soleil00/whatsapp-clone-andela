93a425cf1ee665c707b6179fd7be692df1cb872f
soleil00 <srukundo01@gmail.com>
2024-02-02 15:27:50 +0200

    edited next.config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // Your other Next.js configuration options go here
 
-module.exports = nextConfig
+  // Add the output property for static HTML export
+  output: {
+    // This will create a folder named "out" with the exported files
+    // Feel free to change the "out" to your preferred output folder name
+    dir: 'out',
+  },
+};
+
+module.exports = nextConfig;
